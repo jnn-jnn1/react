7aec5335182e90d6775aef32425f0c1f0e951c12
Jin Jin <anj72005@gmail.com>
2024-05-09 14:33:20 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,4 @@
 function App() {
-  // &&, ||
-
-  // falsy(false 같은) : false, 0, "", null, undefined
-
-  const a = undefined;
-  if (a) {
-    console.log("a는 참이다");
-  } else {
-    console.log("a는 거짓이다");
-  }
-
-  let b = 0;
-
-  let c = b && "값이 있음";
-  console.log("c", c); // 0
-
-  let d = b || "값이 없음";
-  console.log("d", d); // 값이 없음
-
-  // truthy (true 같은) : falsy 가 아니면 truthy
-
   return <div></div>;
 }
 
