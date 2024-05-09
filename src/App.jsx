aef421571517b4f761606865d95a230e89ad10d9
Jin Jin <anj72005@gmail.com>
2024-05-09 15:10:43 +0900

    javascript object와 property

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  const a = { name: "흥민", team: "토트넘", salary: "500" };
+  return (
+    <div>
+      {a.name}은 {a.team} 소속이고 연봉은 {a.salary}원 이다.
+    </div>
+  );
 }
 
 export default App;
